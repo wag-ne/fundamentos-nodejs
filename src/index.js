5f1e16dbae8f6aeaf4aff717255814c8a06039f1
Wagner Santos <wagnersantos@pop-os.localdomain>
2022-04-25 20:46:06 -0300

    add nodemon

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.json({ message: "Hello world!" });
+  return response.json({ message: "Hello world 2!" });
 });
 
 app.listen(3333);
