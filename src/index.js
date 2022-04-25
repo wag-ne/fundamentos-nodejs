4c7825b3259066ff0749760b61e670af269677a5
Wagner Santos <wagnersantos@pop-os.localdomain>
2022-04-25 20:56:55 -0300

    add new routes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello world 2!" });
+app.get("/courses", (request, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post("/courses", (request, response) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 2", "Curso 3"]);
+});
+
+app.patch("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 7", "Curso 3"]);
+});
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json(["Curso 6", "Curso 7"]);
 });
 
 app.listen(3333);
